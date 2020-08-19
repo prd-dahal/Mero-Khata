b3f8419b8e102b96e8e388fe8f542e6b432433d9
Pradeep Dahal <pradeep.dahal222@gmail.com>
2020-08-19 16:44:24 +0545

    final initial setup

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 const express = require('express')
+const bodyParser = require('body-parser')
+const ejs = require('ejs')
+
 app = express()
+app.use(bodyParser.urlencoded({extended:true}))
+app.set('view engine','ejs')
+app.use(express.static('public'))
 
 
 app.get('/',function(req,res){
-  res.send("Hello World")
+  res.render('home',{name:"Pradeep"})
 })
 app.listen(3000,function(){
   console.log('Server started at http://localhost:3000')
